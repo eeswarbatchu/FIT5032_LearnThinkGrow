c9707083e646b1c9d0296ace1b09954115dfd36e
Eeswar Batchu <ebat0007@student.monash.edu>
2020-10-03 15:29:18 +1000

    Add project files.

---
+++ FIT5032_LearnThinkGrow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_LearnThinkGrow.Startup))]
+namespace FIT5032_LearnThinkGrow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
